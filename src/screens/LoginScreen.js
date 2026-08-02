c92279e97f17193c6815c6446e9a923077146854
sajin-cl <sajincl222@gmail.com>
2026-08-02 23:37:33 +0530

    login screen email field added

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,32 +1,45 @@
-import { View, Text, TextInput } from "react-native";
+import { View, Text, TextInput, StyleSheet } from "react-native";
 
 export default function LoginScreen() {
-  return (
-    <View
-      style={{
+    return (
+        <View
+            style={styles?.container}
+        >
+            <Text
+                style={styles?.greetingText}
+            >Welcome Back</Text>
+
+            <TextInput
+                placeholder="Enter your email"
+                style={styles?.emailField}
+                keyboardType="email-address"
+                autoCapitalize="none"
+                autoCorrect={false}
+            />
+        </View>
+
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
         flex: 1,
         justifyContent: "center",
         alignItems: "center",
-        padding:20
-      }}
-    >
-      <Text
-      style={{
-        fontSize:28,
-        fontWeight:'bold',
-        marginBottom:20
-      }}
-      >Welcome Back</Text>
+        padding: 20
+    },
 
-      <TextInput
-      placeholder="Enter your Email..."
-      style={{
-        borderWidth:1,
-        borderColor:'blue',
-        padding:15,
-        borderRadius:12
-      }}
-      />
-    </View>
-  );
-}
+    greetingText: {
+        fontSize: 28,
+        fontWeight: 'bold',
+        marginBottom: 20
+    },
+    emailField: {
+        borderWidth: 1,
+        borderColor: '#ccc',
+        padding: 15,
+        borderRadius: 12,
+        width: '100%'
+    }
+
+});
