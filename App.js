c3fe494c92d17c41005f0fdd50895009849d80ae
sajin-cl <sajincl222@gmail.com>
2026-08-02 22:50:29 +0530

    UI layout concept using flex:1

--- App.js
+++ App.js
@@ -2,28 +2,21 @@ import { View } from "react-native";
 
 export default function App() {
   return (
-    <>
-      <View
-        style={{
-          flex: 1,
-          backgroundColor: "red",
-        }}
-      />
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: "#ffffff",
+      }}
+    >
+      <View style={{ flex: 4, backgroundColor: "#f5f5f5" }} />
 
-      <View
-        style={{
-          flex: 2,
-          backgroundColor: "green",
-        }}
-      />
+      <View style={{ flex: 1, backgroundColor: "#ffffff" }} />
 
-      <View
-        style={{
-          flex: 1,
-          backgroundColor: "blue",
-        }}
-      />
-    </>
+      <View style={{ flex: 3, backgroundColor: "#eeeeee" }} />
 
+      <View style={{ flex: 1, backgroundColor: "#ffffff" }} />
+
+      <View style={{ flex: 1, backgroundColor: "#dddddd" }} />
+    </View>
   );
-} 
+}
