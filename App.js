b52123c480b466a1b556d7a3a25b0dcda63bd3dc
sajin-cl <sajincl222@gmail.com>
2026-08-02 21:47:25 +0530

    practical one

--- App.js
+++ App.js
@@ -4,7 +4,9 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.heading}>MY PROFILE</Text>
+      <Text style={styles.title}>Sajin.C.L</Text>
+      <Text style={styles.jobRole}>MERN STACK DEVELOPER</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +15,20 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'white',
+    color:'white',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  title: {
+    fontSize: 30,
+    fontWeight: "bold",
+  },
+  heading:{
+    color:"blue",
+  },
+  jobRole:{
+    color:'gray',
+    justifyContent:'center'
+  }
 });
