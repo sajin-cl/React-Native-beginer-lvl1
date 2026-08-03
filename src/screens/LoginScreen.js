4aee42147e042698691f5790c76669d05513e817
sajin-cl <sajincl222@gmail.com>
2026-08-03 22:47:33 +0530

    email ,password error message added

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -6,17 +6,73 @@ export default function LoginScreen() {
     const [isPasswordHidden, setIsPasswordHidden] = useState(true);
     const [password, setPassword] = useState('');
     const [email, setEmail] = useState('');
+    const [errors, setErrors] = useState({
+        email: "",
+        password: "",
+    })
 
 
     const togglePasswordVisiblity = () => {
         setIsPasswordHidden(prev => !prev)
     };
 
+    //for email
+    const handleEmailChange = (text) => {
+        setEmail(text);
+
+        setErrors(prev => ({
+            ...prev,
+            email: ""
+        }));
+    };
+
+    //for password
+    const handlePasswordChange = (text) => {
+        setPassword(text);
+
+        setErrors(prev => ({
+            ...prev,
+            password: ""
+        }));
+    };
+
 
     const handleLogin = () => {
+
+        const newErrors = {
+            email: '', password: ''
+        }
+
+        //Email validation
+        if (!email.trim()) {
+            newErrors.email = 'email is required'
+        }
+        else if (!email.includes('@')) {
+            newErrors.email = 'email is not valid'
+        }
+
+        //Password validation
+        if (!password.trim()) {
+            newErrors.password = "Password is required";
+        }
+        else if (password.length < 8) {
+            newErrors.password = 'Password should be at least 8 characters long'
+        }
+
+        setErrors(newErrors);
+
+        console.log("Login failed");
+
+        if (newErrors.email || newErrors.password) {
+            return;
+        }
+
         console.log("Login Successfully");
-        console.log({email,password});
+        console.log({ email, password });
+
     }
+
+
 
     return (
         <View
@@ -26,25 +82,44 @@ export default function LoginScreen() {
                 Welcome Back
             </Text>
 
-            <TextInput
-                placeholder="Enter your email"
-                style={styles.inputBox}
-                keyboardType="email-address"
-                autoCapitalize="none"
-                autoCorrect={false}
-                value={email}
-                onChangeText={setEmail}
-            />
 
-            <TextInput
-                placeholder="Enter your password"
-                secureTextEntry={isPasswordHidden}
-                autoCorrect={false}
-                autoCapitalize="none"
-                style={styles.inputBox}
-                value={password}
-                onChangeText={setPassword}
-            />
+            <View style={styles.inputContainer}>
+                <TextInput
+                    placeholder="Enter your email"
+                    style={styles.inputBox}
+                    keyboardType="email-address"
+                    autoCapitalize="none"
+                    autoCorrect={false}
+                    value={email}
+                    onChangeText={handleEmailChange}
+                />
+                {errors.email && (
+                    <Text style={styles.errorMessage}>
+                        {errors.email}
+                    </Text>
+                )}
+            </View>
+
+
+            <View style={styles.inputContainer}>
+                <TextInput
+                    placeholder="Enter your password"
+                    secureTextEntry={isPasswordHidden}
+                    autoCorrect={false}
+                    autoCapitalize="none"
+                    style={styles.inputBox}
+                    value={password}
+                    onChangeText={handlePasswordChange}
+                />
+                {errors.password && (
+                    <Text style={styles.errorMessage} >
+                        {errors.password}
+                    </Text>
+                )}
+
+            </View>
+
+
             <Pressable onPress={togglePasswordVisiblity} >
                 <Text>
                     {isPasswordHidden ? "Show Password" : "Hide Password"}
@@ -58,8 +133,8 @@ export default function LoginScreen() {
             </Pressable>
         </View>
 
-    );
-}
+    )
+};
 
 const styles = StyleSheet.create({
     container: {
@@ -74,13 +149,17 @@ const styles = StyleSheet.create({
         fontWeight: 'bold',
         marginBottom: 20
     },
+    inputContainer: {
+        width: '100%',
+        marginBottom: 10
+    },
     inputBox: {
         borderWidth: 1,
         borderColor: '#ccc',
         padding: 15,
         borderRadius: 12,
         width: '100%',
-        marginBottom: 20
+        marginBottom: 10
     },
     loginBtn: {
         marginTop: 10,
@@ -94,6 +173,11 @@ const styles = StyleSheet.create({
         color: "white",
         fontSize: 16,
         fontWeight: "600",
+    },
+    errorMessage: {
+        color: 'red',
+        marginBottom: 10,
+        alignSelf: 'flex-start'
     }
 
-});
+})
