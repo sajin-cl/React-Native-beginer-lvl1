e2ce182b4b9cf4e70bbdd2e844be4759f3a61b1a
sajin-cl <sajincl222@gmail.com>
2026-08-03 21:00:22 +0530

    Login button added , pressed , onChangeText

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -4,11 +4,19 @@ import { View, Text, TextInput, StyleSheet, Pressable } from "react-native";
 export default function LoginScreen() {
 
     const [isPasswordHidden, setIsPasswordHidden] = useState(true);
+    const [password, setPassword] = useState('');
+    const [email, setEmail] = useState('');
 
 
     const togglePasswordVisiblity = () => {
         setIsPasswordHidden(prev => !prev)
     };
+
+
+    const handleLogin = () => {
+        console.log("Login Successfully");
+        console.log({email,password});
+    }
 
     return (
         <View
@@ -24,19 +32,29 @@ export default function LoginScreen() {
                 keyboardType="email-address"
                 autoCapitalize="none"
                 autoCorrect={false}
+                value={email}
+                onChangeText={setEmail}
             />
 
             <TextInput
-                placeholder="password"
+                placeholder="Enter your password"
                 secureTextEntry={isPasswordHidden}
                 autoCorrect={false}
                 autoCapitalize="none"
                 style={styles.inputBox}
+                value={password}
+                onChangeText={setPassword}
             />
-            <Pressable onPress={togglePasswordVisiblity}>
+            <Pressable onPress={togglePasswordVisiblity} >
                 <Text>
                     {isPasswordHidden ? "Show Password" : "Hide Password"}
                 </Text>
+            </Pressable>
+
+            <Pressable
+                onPress={handleLogin}
+                style={({ pressed }) => [styles.loginBtn, { backgroundColor: pressed ? '#1D4ED8' : '#2563EB' }]}>
+                <Text style={styles.buttonText}>Login</Text>
             </Pressable>
         </View>
 
@@ -63,6 +81,19 @@ const styles = StyleSheet.create({
         borderRadius: 12,
         width: '100%',
         marginBottom: 20
+    },
+    loginBtn: {
+        marginTop: 10,
+        paddingVertical: 15,
+        borderRadius: 12,
+        width: '100%',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    buttonText: {
+        color: "white",
+        fontSize: 16,
+        fontWeight: "600",
     }
 
 });
